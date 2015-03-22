79f332c9d30788ceea11f5fee22ceb5f004181cc
umanhay <ptown727@yahoo.com>
2015-03-22 13:19:16 -0700

    question view and template

--- app/views.py
+++ app/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 from app.models import Question
 
 def question(request):
-	questions = Question.objects.order_by('published')
-	print(questions)
-	return render(request, 'app/index.html', {'questions': questions})
-
+	# questions = Question.objects.order_by('published')
+	try:
+		qid = int(request.GET.get("p"))
+	except:
+		qid = 1 
+	question = Question.objects.get(id=qid) 
+	text = 	question.question
+	# qid = question.id
+	nextid = qid + 1
+	lastid = qid - 1
+	print(text)
+	return render(request, 'app/index.html', {'question': text, 'qid': qid, 'nextid': nextid, 'lastid': lastid})
 
 
