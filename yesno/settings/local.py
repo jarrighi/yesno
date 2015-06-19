e2fab46c2893f62a196d0f7424bee5fbcb61f917
umanhay <ptown727@yahoo.com>
2015-06-18 19:10:55 -0700

    modify gitignore

--- yesno/settings/local.py
+++
@@ -1,8 +0,0 @@
-from .base import BASE_DIR
-
-SECRET_KEY = 'q(5g^54v=%8nq7^#0=w0^j+ee4_s1+e+jno@zg3gyoqsclqf8c'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
