602fc6e4838024b1fe69317abae2fc07b319ced6
Jules Arrighi <juliana.arrighi@gmail.com>
2015-12-02 19:23:49 -0800

    Add a password confirmation field. Needs to be refactored

--- app/forms.py
+++ app/forms.py
@@ -2,13 +2,27 @@ from django import forms
 from app.models import User
 
 class UserForm(forms.ModelForm):
-	username = forms.CharField(min_length=5, max_length=16, required=True, 
-					help_text="Choose your Ninja name (must be at least 5 characters long)")
-	email = forms.EmailField(required=True, help_text="Enter your email address")
-	password = forms.CharField(min_length=8, max_length=32, widget=forms.PasswordInput, required=True,
-					help_text="Create your secret password- greater than 7 characters")
+  username = forms.CharField(help_text="Choose your Ninja name (must be at least 5 characters long)",
+                              min_length=5, 
+                              max_length=16, 
+                              required=True)
 
-	class Meta:
-        # Provide an association between the ModelForm and a model
-	   model = User
-	   fields = ('username', 'email', 'password')
+  email = forms.EmailField(help_text="Enter your email address",
+                              required=True)
+
+  password1 = forms.CharField(help_text="Create your secret password- greater than 7 characters",
+                              min_length=8, 
+                              max_length=32, 
+                              widget=forms.PasswordInput, 
+                              required=True)
+
+  password2 = forms.CharField(help_text="Confirm your password",
+                              min_length=8, 
+                              max_length=32, 
+                              widget=forms.PasswordInput, 
+                              required=True)
+
+  class Meta:
+    # Provide an association between the ModelForm and a model
+    model = User
+    fields = ('username', 'email')
