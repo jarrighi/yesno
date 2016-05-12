6197c1ed1ad87247a63274bdb1f0b4061bc733a7
umanhay <ptown727@yahoo.com>
2016-05-11 17:30:27 -0700

    Add Bootstrap Fonts directory to project

---
+++ staticfiles/js/answer.js
@@ -0,0 +1,25 @@
+var answerButtons = document.querySelectorAll(".answer .btn");
+for (var i = 0; i < answerButtons.length; i++) {
+    answerButtons[i].addEventListener('click', function() {
+        var choice = this.dataset.choice;
+        var endpoint = '/answer/' + qid + "/" + choice;
+        $.ajax({
+            url: endpoint,
+            error: function() {
+                console.log("Ajax request failed.");
+            },
+            success: function(data) {
+                var percentYes = Math.round((data.yesses / ( data.yesses + data.nos)) * 100);
+                var percentNo = (100 - percentYes);
+
+                var data_html = '<div class="record"><div class="bar" style="width:' + percentYes
+                + '%;"><span>Yes</span></div><div class="p"><span>' + data.yesses
+                + '</span></div></div><div class="record"><div class="bar" style="width:'
+                + percentNo + '%;"><span>No</span></div><div class="p"><span>'
+                + data.nos + '</span></div></div>';
+
+                $('#data').html(data_html);
+            }
+        });
+    });
+};
